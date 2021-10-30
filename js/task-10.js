95d9202ed64db4bb9b48b764f44cc45a978d1a1d
forysiak-vova <volodja.forus@gmail.com>
2021-10-30 12:42:19 +0200

    rt

--- js/task-10.js
+++ js/task-10.js
@@ -37,36 +37,41 @@ function getRandomHexColor() {
 //     return Math.floor(Math.random() * 256);
 //   }
 // }
-
- const render = document.querySelector('.data-create');
- const destroy = document.querySelector('.data-destroy');
-const boxes = document.querySelector("#boxes");
- 
-render.addEventListener('click', getAmount);
-
-destroy.addEventListener('click', destroyBoxes);
+const refs = {
+    createBtn: document.querySelector('[data-action="render"]'),
+    destroyBtn: document.querySelector('[data-action="destroy"]'),
+    divBoxes: document.querySelector('#boxes'),
+    input: document.querySelector('input'),
+}
+refs.createBtn.addEventListener('click', getAmount)
+refs.destroyBtn.addEventListener('click', destroyBoxes)
 
 function getAmount() {
-  const amount = +document.querySelector("#controls input").value;
+  const amount = refs.input.value
   createBoxes(amount)
+}
 
-  function createBoxes(amount) {
-    const basicSize = 30;
-    const fragment = document.createDocumentFragment();
-    for (const i = 0; i < amount; i++) {
-      const size = basicSize + i * 10;
-      const div = document.createElement("div");
-      div.style.cssText = `width: ${size}px; height: ${size}px; background-color: rgba( ${random()} , ${random()} , ${random()} )`;
-      fragment.appendChild(div);
+function destroyBoxes() {
+    refs.divBoxes.textContent = ''
+    refs.input.value = ''
+} 
+function createBoxes(amount) {
+    const boxSize = 30
+    const element = document.createDocumentFragment()
+    for (let i = 0; i < amount; i+=1) {
+        let size = boxSize + i * 10
+        const divEl = document.createElement('div')
+
+revs.inputEL.addEventListener('input') 
+        divEl.style.cssText = `
+        width: ${size}px;
+        height: ${size}px; 
+        background-color: rgb(
+            ${Math.floor(Math.random() * 255)} ,
+            ${Math.floor(Math.random() * 255)} ,
+            ${Math.floor(Math.random() * 255)} )`
+        element.append(divEl)
     }
-    boxes.appendChild(fragment);
-  }
+    boxes.append(element)
 
-  function destroyBoxes() {
-    boxes.innerHTML = "";
-  }
-
-  function random() {
-    return Math.floor(Math.random() * 256);
-  }
 }
